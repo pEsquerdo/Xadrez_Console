4020693a8b1c4211f3d07833d001b8031be34f5b
Pedro Esquerdo <pedroesquerdo87@gmail.com>
2020-04-15 00:57:27 -0300

    Começando a implementar o tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QntMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+            QntMovimentos = 0;
+        }
+    }
+}
